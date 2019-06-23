5186c032a912204617463b94007161d877c945c6
OcaenEyes <1450136519@qq.com>
2019-06-23 15:18:27 +0800

    Create python_sqlite_fetchone-fetchmany-fetchall.py

---
+++ 数据库操作/python_sqlite_fetchone-fetchmany-fetchall.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+'''
+    auth:gzy
+    date:20190620
+    version:0.1.0
+'''
+import sqlite3
+
+# fetchone() 用于获取一条记录，
+# fetchmany(n) 用于获取 n 条记录，
+# fetchall() 用于获取全部记录。
