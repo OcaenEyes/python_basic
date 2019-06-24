1b0f2a50f23cfbee5f95c33a1422a5454260fd0d
OcaenEyes <1450136519@qq.com>
2019-06-24 18:54:06 +0800

    Create python_线程生命周期.py

---
+++ 并发(多线程多进程)/python_线程生命周期.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+'''
+    auth:gzy
+    date:20190623
+    version:0.1.0
+'''
+import threading
+'''
+    线程的生命周期:
+        新建 new
+        就绪 ready
+        运行 running
+        阻塞 blocked
+        死亡 dead
+'''
